650c7f6df81279b6e8a39bcfc1c1df146ae7a752
Tushar Rai <tusharrai132@gmail.com>
2021-12-26 17:30:05 +0530

    Code

--- GnericTree/Constructor.java
+++ GnericTree/Constructor.java
@@ -2,11 +2,15 @@ import java.util.*;
 
 public class Constructor {
 
+    private static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
   public static void main(String[] args) {
     // Write your code here
     int[] arr ={10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
     Stack<Node> st = new Stack();
-    Node root;
+    Node root = null;
 
     for(int i=0;i<arr.length;i++){
         if(arr[i]==-1){
