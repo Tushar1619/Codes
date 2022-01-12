62e8945216e77c8e4bef3a627a9110bf05b00b81
Tushar Rai <tusharrai132@gmail.com>
2022-01-12 13:04:30 +0530

    Code

---
+++ Binay Search Tree/Intro.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Intro {
+
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        //constructor
+        Node(int data,Node left,Node right){
+            this.data=data;
+            this.left=left;
+            this.right=right;
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
