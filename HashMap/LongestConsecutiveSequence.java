6f834c7511f49896009829b10702dc0361a31917
Tushar Rai <tusharrai132@gmail.com>
2022-01-15 23:43:41 +0530

    Code

---
+++ HashMap/LongestConsecutiveSequence.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+import java.io.*;
+import java.util.*;
+
+public class LongestConsecutiveSequence{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    for(int i=0;i<n;i++){
+        a[i]=sc.nextInt();
+    }
+
+    HashMap<Integer,Boolean> hm = new HashMap<>();
+
+    for(int key:a){
+        hm.put(key,true);
+    }
+
+    for(int key:a){
+        if(hm.containsKey(key-1)){
+            hm.put(key,false);
+        }     
+    }
+
+    int mc=0;
+    int mk=0;
+    for(int key:a){
+        if(hm.get(key)){
+            int tc=1;
+            int tk=key;
+
+            while(hm.containsKey(tk+tc)){
+                tc++;
+            }
+            if(tc>mc){
+            mc=tc;
+            mk=tk;
+        }
+        }
+
+        
+    }
+
+    for(int i=0;i<mc;i++){
+        System.out.println(mk+i);
+    }
+ }
+
+}
+
